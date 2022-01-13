ab8abcf4447bcc5ad519f625956c4e5f51419061
Gustavo Sousa <gustavohl98@gmail.com>
2022-01-13 20:46:36 -0300

    Initial commit

---
+++ src/main/java/com/errors/center/ErrorsCenterApplication.java
@@ -0,0 +1,13 @@
+package com.errors.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorsCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorsCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/errors/center/ErrorsCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errors.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorsCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
