0bf8683ac57fb2a1f0367c250ff89fa5ce781384
Gustavo Sousa <gustavohl98@gmail.com>
2022-01-22 15:59:41 -0300

    Initial implementation of LogEventController

--- src/main/java/com/errors/center/controller/LogEventController.java
+++ src/main/java/com/errors/center/controller/LogEventController.java
@@ -1,4 +1,34 @@
 package com.errors.center.controller;
 
+import com.errors.center.model.LogEvent;
+import com.errors.center.service.LogEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/logevent")
 public class LogEventController {
+
+    @Autowired
+    private LogEventService logEventService;
+
+    @PostMapping
+    public LogEvent save(@RequestBody LogEvent logEvent) {
+        return this.logEventService.save(logEvent);
+    }
+
+    @GetMapping
+    public Iterable<LogEvent> findAll() {
+        return this.logEventService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public LogEvent findById(@PathVariable("id") Long id) {
+        return this.logEventService.findById(id).get();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        this.logEventService.deleteById(id);
+    }
 }
